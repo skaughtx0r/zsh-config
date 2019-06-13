816c9bc74917d424b47e9e6c71852e409592b2f7
Scott Przybylski <skaughtx0r@gmail.com>
2019-06-13 16:16:34 -0700

    Use zsh as default shell

--- install-oh-my-zsh.sh
+++ install-oh-my-zsh.sh
@@ -4,9 +4,12 @@ source $HOME/.zshrc
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 git clone https://github.com/bhilburn/powerlevel9k.git  $ZSH_CUSTOM/themes/powerlevel9k
+
+# Install powerline font with icons
 mkdir -p $HOME/.local/share/fonts
 curl -fLo "$HOME/.local/share/fonts/Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
 fc-cache -f -v
+
 sudo ln -s /etc/profile.d/vte-2.91.sh /etc/profile.d/vte.sh
 
 cd $(dirname "$0")
@@ -21,3 +24,5 @@ for i in `\ls -ad1 $DOTDIR\.* | grep -v -e ".git"`; do
 done
 
 ln -s $DOTDIR .dotfiles
+
+chsh -s /usr/bin/zsh
