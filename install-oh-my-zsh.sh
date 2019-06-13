4a3438f7715106da9d63e349b23d30569a22c255
Scott Przybylski <skaughtx0r@gmail.com>
2019-06-13 14:25:33 -0700

    Script fixes.

--- install-oh-my-zsh.sh
+++ install-oh-my-zsh.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/env zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh) --unattended"
-source ~/.zshrc
+source $HOME/.zshrc
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+git clone https://github.com/bhilburn/powerlevel9k.git  $ZSH_CUSTOM/themes/powerlevel9k
 mkdir -p $HOME/.local/share/fonts
 curl -fLo "$HOME/.local/share/fonts/Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
+
+cd $(dirname "$0")
+DOTDIR="$(pwd)/"
+
+cd $HOME
+
+for i in `\ls -ad1 $DOTDIR\.* | grep -v -e ".git"`; do
+  j=$(basename $i)
+  mv $j $j.old 2>/dev/null
+  ln -s .dotfiles/$j . 2>/dev/null
+done
+
+ln -s $DOTDIR .dotfiles
